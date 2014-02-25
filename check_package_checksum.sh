dfe33c4b2d030107c4de54ae881c1832a74918a3
bsuiram <marius.berntsberg@gmail.com>
2014-02-25 09:38:34 +0100

    cleanup

--- check_package_checksum.sh
+++ check_package_checksum.sh
@@ -6,11 +6,11 @@ distro=$(lsb_release -a 2> /dev/null | awk '/Distributor/ {print $3}')
 case ${distro} in
   Ubuntu|Debian )
     index=1
-    pkgmgr="check_dpkg"
+    pkgmgr="dpkg"
     ;;
   RedHat|CentOS|Scientific )
     index=2
-    pkgmgr="check_rpm"
+    pkgmgr="rpm"
     ;;
   * )
     echo "Unknown Distro: Please fix me.. im broken.."
@@ -24,53 +24,48 @@ esac
 #echo "  and ${pkgmgr} as package manager."
 
 #  Array of pseudo binary names to check
-checks=( BASH SSHD LOGIN SU SUDO )
+checks=( pBASH pSSHD pLOGIN pSU pSUDO )
 
 # Packages:
 # pseudo_binary_name=( '/full/path/to/binary' 'deb-package-name' 'rpm-package-name' 'binary_name' )
-BASH=( '/bin/bash' 'bash' 'bash' 'bash' )
-SSHD=( '/usr/sbin/sshd' 'openssh-server' 'openssh-server' 'sshd' )
-LOGIN=( '/bin/login' 'login' 'util-linux-ng' 'login' )
-SU=( '/bin/su' 'login' 'coreutils' 'su' )
-SUDO=( '/usr/bin/sudo' 'sudo' 'sudo' 'sudo' )
+pBASH=( '/bin/bash' 'bash' 'bash' 'bash' )
+pSSHD=( '/usr/sbin/sshd' 'openssh-server' 'openssh-server' 'sshd' )
+pLOGIN=( '/bin/login' 'login' 'util-linux-ng' 'login' )
+pSU=( '/bin/su' 'login' 'coreutils' 'su' )
+pSUDO=( '/usr/bin/sudo' 'sudo' 'sudo' 'sudo' )
 
-check_dpkg () {
+checksums () {
   # Takes 2 arguments;
   #  1) path from pseudo_binary_name array element #1
   #  2) package name from pseudo_binary_name array determined by distribution ${index}
   #  Returns: 0 if checksums are identical, and 1 if they differ.
-  binary_path=${1:1:${#1}} #Remove first char from path (/) as ${package_name}.md5sums contains path without starting /.
-  package_name=${2}
-
-  package_md5sum=$(cat /var/lib/dpkg/info/${package_name}.md5sums | egrep "${binary_path}$" | awk '{print $1}')
-  binary_md5sum=$(md5sum /${binary_path} | awk '{print $1}')
-
-  if [ ${binary_md5sum} == ${package_md5sum} ]; then
-    return 0
-  else
-    return 1
-  fi
-}
-
-check_rpm () {
-  # Takes 2 arguments;
-  #  1) path from pseudo_binary_name array element #1
-  #  2) package name from pseudo_binary_name array determined by distribution, ${index}
-  #  Returns: 0 if checksums are identical, and 1 if they differ.
   binary_path=${1}
   package_name=${2}
 
-  package_sha256sum=$(rpm -ql --dump ${package_name} | grep "${binary_path} " | awk '{print $4}')
-  binary_sha256sum=$(sha256sum ${binary_path} | awk '{print $1}')
+  case ${pkgmgr} in
+    dpkg )
+      # Remove first char from path (/)
+      # as ${package_name}.md5sums contains path without starting /
+      binary_path_mod=${binary_path:1:${#binary_path}}
 
-  if [ ${binary_sha256sum} == ${package_sha256sum} ]; then
+      package_checksum=$(cat /var/lib/dpkg/info/${package_name}.md5sums | egrep "${binary_path_mod}$" | awk '{print $1}')
+      binary_checksum=$(md5sum ${binary_path} | awk '{print $1}')
+      ;;
+    rpm )
+       package_checksum=$(rpm -ql --dump ${package_name} | egrep "^${binary_path} " | awk '{print $4}')
+       binary_checksum=$(sha256sum ${binary_path} | awk '{print $1}')
+      ;;
+    * )
+      ;;
+  esac
+
+  if [ ${binary_checksum} == ${package_checksum} ]; then
     return 0
   else
     return 1
   fi
 }
 
-# Check sums of binarys against package.
 do_checks () {
 
   failed=0
@@ -79,11 +74,13 @@ do_checks () {
   for element in ${checks[@]}; do
     eval "prog=(\${$element[@]})"
 
-    ${pkgmgr} ${prog[0]} ${prog[${index}]}
+    checksums ${prog[0]} ${prog[${index}]}
+
     if [ $? -ne 0 ]; then
       failed_binarys[${failed}]=${prog[0]}
+      failed_packages[${failed}]=${prog[${index}]}
       failed=$((failed+1))
-      echo "CRITICAL: Package and binary checksum differs for ${prog[0]} in package \"${prog[${index}]}\"!"
+
     else
       not_failed_binarys[${not_failed}]=${prog[0]}
       not_failed=$((not_failed+1))
@@ -91,7 +88,9 @@ do_checks () {
   done
 
   if [ ${failed} -ne 0 ]; then
-    echo "CRITICAL: Verification of binary/package checksum failed for ${failed_binarys[@]}!"
+    echo "CRITICAL: Verification of binar vs. pckage checksum failed!"
+    echo "Affcted binarys: ${failed_binarys[@]}"
+    echo "Affected packages: ${failed_packages[@]}"
     exit 2
   else
     echo "OK: Package and binary checksum are identical for ${not_failed_binarys[@]}."
