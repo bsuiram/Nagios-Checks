ffe08da83c0a853d88ee64cae2b74382a03be1b9
bsuiram <marius.berntsberg@gmail.com>
2014-02-27 13:45:17 +0100

    fixed missing count variable

--- check_package_checksum.sh
+++ check_package_checksum.sh
@@ -260,11 +260,13 @@ do_checks () {
 output () {
 
   nagios_error=0
+  local count=0
 
   if [ ${#failed_files[@]} -ne "0" ]; then
     echo "CRITICAL: Verification of file vs. package checksum failed!"
     for i in ${failed_files[@]}; do
         echo " ${failed_files[${count}]} doen not match ${failed_packages[${count}]}"
+        let count=count+1
     done
     nagios_error=2
   elif [ ${#orphaned_files[@]} -ne "0" ]; then
